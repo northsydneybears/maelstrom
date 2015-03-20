c29bafa0e384f6e1bfa46e3d98a19ed6fb7259dd
Peter Gow <peter.gow@gmail.com>
2015-03-21 05:41:26 +1100

    Initial Commit

---
+++ Bob/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  Bob
+//
+//  Created by Peter Gow on 21/03/2015.
+//  Copyright (c) 2015 Peter Gow. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Bob/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Bob
+//
+//  Created by Peter Gow on 21/03/2015.
+//  Copyright (c) 2015 Peter Gow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
